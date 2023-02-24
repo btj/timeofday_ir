1d4f193322cf17d2f3eab9550d99ab6c3fbce787
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-02-24 16:29:48 +0100

    Implementatie met hours en minutes

--- timeofday_ir/src/timeofday_ir/TimeOfDay.java
+++ timeofday_ir/src/timeofday_ir/TimeOfDay.java
@@ -11,15 +11,37 @@ package timeofday_ir;
 // 3. Declareren en documenteren
 //    van constructoren en mutatoren
 
+// Stappenplan implementatie data-abstractie
+
+// 1. Velden definiëren =
+//    rauwe concretetoestandsruimte definiëren
+//
+// 2. Geldige concretetoestandsruimte definiëren
+//    = representatie-invarianten noteren
+//
+// 3. Afbeelding concrete toestand op abstracte toestand
+//    = getters implementeren
+//    Daarbij moet gelden dat elke geldige concrete toestand
+//    afgebeeld wordt op een geldige abstracte toestand
+//
+// 4. Implementeren van constructoren en mutatoren
+
 /**
  * @invar | 0 <= getHours() && getHours() <= 23
  * @invar | 0 <= getMinutes() && getMinutes() <= 59
  */
 public class TimeOfDay {
-
-	public int getHours() { throw new RuntimeException("Not yet implemented"); }
 	
-	public int getMinutes() { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * @invar | 0 <= hours && hours <= 23
+	 * @invar | 0 <= minutes && minutes <= 59
+	 */
+	private int hours;
+	private int minutes;
+
+	public int getHours() { return hours; }
+	
+	public int getMinutes() { return minutes; }
 	
 	/**
 	 * @throws IllegalArgumentException | hours < 0 || 23 < hours
@@ -28,7 +50,15 @@ public class TimeOfDay {
 	 * @post | getHours() == hours
 	 * @post | getMinutes() == minutes
 	 */
-	public TimeOfDay(int hours, int minutes) { throw new RuntimeException("Not yet implemented"); }
+	public TimeOfDay(int hours, int minutes) {
+		if (hours < 0 || 23 < hours)
+			throw new IllegalArgumentException("`hours` out of range");
+		if (minutes < 0 || 59 < minutes)
+			throw new IllegalArgumentException("`minutes` out of range");
+		
+		this.hours = hours;
+		this.minutes = minutes;
+	}
 	
 	/**
 	 * @pre | 0 <= hours && hours <= 23
@@ -36,7 +66,9 @@ public class TimeOfDay {
 	 * @post | getHours() == hours
 	 * @post | getMinutes() == old(getMinutes())
 	 */
-	public void setHours(int hours) { throw new RuntimeException("Not yet implemented"); }
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
 	
 	/**
 	 * @pre | 0 <= minutes && minutes <= 59
@@ -44,5 +76,7 @@ public class TimeOfDay {
 	 * @post | getHours() == old(getHours())
 	 * @post | getMinutes() == minutes
 	 */
-	public void setMinutes(int minutes) { throw new RuntimeException("Not yet implemented"); }
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
 }
